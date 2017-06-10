6cc56b4568f9a67477fa503cf92d0682d7f1b2fa
Eddie <eddie.santos.3@gmail.com>
2017-06-09 18:53:57 -0700

    Add SlickDemo object

---
+++ src/main/scala/SlickDemo.scala
@@ -0,0 +1,12 @@
+/**
+  * Created by Eddie on 6/9/17.
+  */
+import slick.jdbc.PostgresProfile.api._
+
+import scala.concurrent.Future
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+object SlickDemo {
+
+}
