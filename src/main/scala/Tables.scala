5eec0c3f27f0fb97970ec965673df1656249ba33
Eddie <eddie.santos.3@gmail.com>
2017-06-09 19:26:37 -0700

    Add Tables object

---
+++ src/main/scala/Tables.scala
@@ -0,0 +1,22 @@
+/**
+  * Created by Eddie on 6/9/17.
+  */
+import java.sql.Date
+import slick.jdbc.PostgresProfile.api._
+
+object Tables {
+
+  class Transactions(tag: Tag) extends Table[Transaction](tag, "transactions") {
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def candidate = column[String]("candidate")
+    def contributor = column[String]("contributor")
+    def contributorState = column[String]("contributor_state")
+    def contributorOccupation = column[Option[String]]("contributor_occupation")
+    def amount = column[Long]("amount")
+    def date = column[Date]("date")
+
+    def * = (id, candidate, contributor, contributorState, contributorOccupation,
+      amount, date) <> (Transaction.tupled, Transaction.unapply)
+  }
+  
+}
